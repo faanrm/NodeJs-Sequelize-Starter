e86434bbeff244dc949d6aaa359591dd8a85860a
faanrm <faniloniainaa21@gmail.com>
2024-02-10 09:23:21 +0300

    up : update sql config

--- src/config/sql.js
+++ src/config/sql.js
@@ -18,6 +18,7 @@ const sequelize = new Sequelize(
   process.env.DB_NAME,
   process.env.DB_USERNAME,
   process.env.DB_PASSWORD,
+  process.env.DB_DIALECT,
   {
     host: process.env.DB_HOST,
     port: process.env.DB_PORT,
