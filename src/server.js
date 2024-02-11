618d7b124719643726a3620f0c022fd3d4a41772
Faniloniaina RAMILISON <faniloiainaa21@gmail.com>
2024-02-11 14:07:02 +0300

    add : adding config db to the main

--- src/server.js
+++ src/server.js
@@ -1,18 +1,27 @@
-import express from "express"
-import cors from "cors"
+import express from "express";
+import cors from "cors";
 import db from './config/sql.js';
 
+const app = express();
+const port = process.env.PORT || 5200;
 
-const app = express()
-const port = process.env.PORT || 5200
+app.use(cors());
 
+//connection to the db
+db.sequelize.authenticate()
+  .then(() => {
+    console.log('Connection to the database has been established successfully.');
+  })
+  .catch(err => {
+    console.error('Unable to connect to the database:', err);
+    process.exit(-1); 
+  });
 
-//db config 
-//const SQL = db 
+//routes & middleware
 app.get("/", (req, res) => {
-    res.send("Hello World ")
-})
+    res.send("Hello World");
+});
 
 app.listen(port, () => {
-    console.log(`Server Running on Port: http://localhost:${port}`)
-})
+    console.log(`Server Running on Port: http://localhost:${port}`);
+});
