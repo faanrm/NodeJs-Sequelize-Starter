9b38ab9d0c4720c1776c14c6cfe3fe321d35cdb2
Faniloniaina RAMILISON <faniloiainaa21@gmail.com>
2024-02-13 22:39:36 +0300

    add : deleteuser

--- src/controllers/UserControllers.js
+++ src/controllers/UserControllers.js
@@ -1,24 +1,43 @@
 import User from "../models/UserModels.js";
 
-export const getAllUser = async(req, res) =>{
+export const getAllUser = async (req, res) => {
     try {
         const user = User.findAll()
-        if(user){
-            return res.status(401).json({msg: 'list of all users', data : user})
+        if (user) {
+            return res.status(401).json({ msg: 'list of all users', data: user })
         }
-        else{
-            return res.status(404).json({msg : 'np'})
+        else {
+            return res.status(404).json({ msg: 'np' })
         }
-        
+
     } catch (error) {
-        res.status(500).json({message : error.message})
+        res.status(500).json({ message: error.message })
     }
 }
-export const createUser = async(req,res )=> {
+export const createUser = async (req, res) => {
     try {
-        const newUser = await User.create(req.body)
-        return res.status(201).json({message : "User created!", data : newUser})
+        const { name } = req.body
+        if (!name) {
+            console.log("name is required");
+        }
+        return res.status(201).json({ message: "User created!", data: newUser })
     } catch (error) {
-        res.status(400).json({message : error.message})
+        res.status(400).json({ message: error.message })
+    }
+}
+
+export const deleteUser = async (req, res) => {
+    const { id } = req.params
+    try {
+        const deletedUser = await User.destroy({
+            where: { user_id: id }
+        })
+        if (deletedUser) {
+            res.status(204).json({message : "user deleted"}).send();
+          } else {
+            res.status(404).json({ message: 'User not found ' });
+          }
+    } catch (error) {
+        res.status(500).json({message : error.message})
     }
 }
--- src/routes/UserRoutes.js
+++ src/routes/UserRoutes.js
@@ -1,9 +1,10 @@
 import express from "express"
-import { getAllUser , createUser } from "../controllers/UserControllers.js"
+import { getAllUser , createUser , deleteUser} from "../controllers/UserControllers.js"
 
 const router = express.Router()
 
 router.get('/allUser',getAllUser)
 router.post('/createUser',createUser)
+router.delete('/deleteUser/:id',deleteUser)
 
 export default router
