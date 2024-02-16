d0ca05ed26bbff21f20f25a7a4401092b317ef94
Faniloniaina RAMILISON <faniloniainaa21@gmail.com>
2024-02-16 16:38:02 +0300

    update models

--- src/models/UserModels.js
+++ src/models/UserModels.js
@@ -11,6 +11,18 @@ const User = db.define('users', {
   name: {
     type: DataTypes.STRING(100),
     allowNull: false
+  },
+  email : {
+    type : DataTypes.STRING(100),
+    allowNull : false , 
+    unique : true ,
+    validate : {
+      isEmail : true
+    }
+  },
+  password : {
+   type :DataTypes.STRING(100),
+   allowNull: false 
   }
 }, {
   timestamps: false,
